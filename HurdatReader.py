a4fe6878ef082d68c90cb8652047c68c47123a92
B Parazin <bparazin1225@gmail.com>
2022-09-01 10:35:54 -0400

    couple comments and some pep8

--- HurdatReader.py
+++ HurdatReader.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import datetime as dt
 import numpy as np
-    
+
+
+# most of these are a result of the hurdat format, linked in the readme
 def cleanInputLine(line):
     return [entry.strip() for entry in line.split(',')]
 
-#Some longitudes are reported as degrees West, even if they are east of prime merdiain which is an odd choice, this fixes this up to be (-180, 180]
+
+# Some longitudes are reported as degrees West, even if they are east of prime merdiain which is an odd choice,
+# this fixes this up to be (-180, 180]
 def handleLon(lon):
-    val = float(lon[:-1]) * -1 if lon[-1]=='W' else float(lon[:-1])
+    val = float(lon[:-1]) * -1 if lon[-1] == 'W' else float(lon[:-1])
     if val < -180:
         val += 360
     return val
+
 
 def readStorm(idcode, name, lines):
     entry_list = pd.DataFrame([cleanInputLine(line) for line in lines])
@@ -18,16 +23,17 @@ def readStorm(idcode, name, lines):
     result['name'] = name
     result['idcode'] = idcode
     resultData = pd.DataFrame()
-    resultData['datetime'] = [dt.datetime(int(day[0:4]),int(day[4:6]),int(day[6:]), int(time[0:2]), int(time[2:4]), tzinfo = dt.timezone.utc) 
-           for day, time in zip(entry_list[0], entry_list[1])]
+    resultData['datetime'] = [
+        dt.datetime(int(day[0:4]), int(day[4:6]), int(day[6:]), int(time[0:2]), int(time[2:4]), tzinfo=dt.timezone.utc)
+        for day, time in zip(entry_list[0], entry_list[1])]
     resultData['record identifier'] = entry_list[2]
     resultData['system status'] = entry_list[3]
-    resultData['lat'] = [float(coord[:-1]) * -1 if coord[-1]=='S' else float(coord[:-1]) for coord in entry_list[4]]
+    resultData['lat'] = [float(coord[:-1]) * -1 if coord[-1] == 'S' else float(coord[:-1]) for coord in entry_list[4]]
     resultData['lon'] = [handleLon(coord) for coord in entry_list[5]]
-    resultData['Max sustained wind (knots)'] = [float(val) if val!='-999' else None for val in entry_list[6]]
+    resultData['Max sustained wind (knots)'] = [float(val) if val != '-999' else None for val in entry_list[6]]
     statusWithSpeed = []
     for i, maxSpeed in enumerate(resultData['Max sustained wind (knots)']):
-        #classify hurricanes by windspeed
+        # classify hurricanes by windspeed
         if maxSpeed >= 137 and resultData['system status'][i] == 'HU':
             statusWithSpeed.append('HU5')
         elif maxSpeed >= 113 and resultData['system status'][i] == 'HU':
@@ -43,52 +49,59 @@ def readStorm(idcode, name, lines):
         else:
             statusWithSpeed.append(resultData['system status'][i])
     resultData['system status'] = statusWithSpeed
-    resultData['Minimum Pressure (millibars)'] = [float(val) if val!='-999' else None for val in entry_list[7]]
-    #all max wind extents are given as (NE, SE, SW, NW)
-    resultData['34 knot wind radii max extent (NM)'] = [(float(val1) if val1!='-999' else None, 
-                                                                  float(val2) if val2!='-999' else None, 
-                                                                  float(val3) if val3!='-999' else None, 
-                                                                  float(val4) if val4!='-999' else None) 
-                                                                  for val1, val2, val3, val4 in zip(entry_list[8], entry_list[9], entry_list[10], entry_list[11])]
-    resultData['50 knot wind radii max extent (NM)'] = [(float(val1) if val1!='-999' else None, 
-                                                                    float(val2) if val2!='-999' else None, 
-                                                                    float(val3) if val3!='-999' else None, 
-                                                                    float(val4) if val4!='-999' else None) 
-                                                                    for val1, val2, val3, val4 in zip(entry_list[12], entry_list[13], entry_list[14], entry_list[15])]
-    resultData['64 knot wind radii max extent (NM)'] = [(float(val1) if val1!='-999' else None, 
-                                                                    float(val2) if val2!='-999' else None, 
-                                                                    float(val3) if val3!='-999' else None, 
-                                                                    float(val4) if val4!='-999' else None) 
-                                                                    for val1, val2, val3, val4 in zip(entry_list[16], entry_list[17], entry_list[18], entry_list[19])]
-    resultData['max wind radius (NM)'] = [val if val !='-999' else None for val in entry_list[20]]
+    resultData['Minimum Pressure (millibars)'] = [float(val) if val != '-999' else None for val in entry_list[7]]
+    # all max wind extents are given as (NE, SE, SW, NW)
+    resultData['34 knot wind radii max extent (NM)'] = [(float(val1) if val1 != '-999' else None,
+                                                         float(val2) if val2 != '-999' else None,
+                                                         float(val3) if val3 != '-999' else None,
+                                                         float(val4) if val4 != '-999' else None)
+                                                        for val1, val2, val3, val4 in
+                                                        zip(entry_list[8], entry_list[9], entry_list[10],
+                                                            entry_list[11])]
+    resultData['50 knot wind radii max extent (NM)'] = [(float(val1) if val1 != '-999' else None,
+                                                         float(val2) if val2 != '-999' else None,
+                                                         float(val3) if val3 != '-999' else None,
+                                                         float(val4) if val4 != '-999' else None)
+                                                        for val1, val2, val3, val4 in
+                                                        zip(entry_list[12], entry_list[13], entry_list[14],
+                                                            entry_list[15])]
+    resultData['64 knot wind radii max extent (NM)'] = [(float(val1) if val1 != '-999' else None,
+                                                         float(val2) if val2 != '-999' else None,
+                                                         float(val3) if val3 != '-999' else None,
+                                                         float(val4) if val4 != '-999' else None)
+                                                        for val1, val2, val3, val4 in
+                                                        zip(entry_list[16], entry_list[17], entry_list[18],
+                                                            entry_list[19])]
+    resultData['max wind radius (NM)'] = [val if val != '-999' else None for val in entry_list[20]]
     result['data'] = resultData
     return result
 
-def readHurdat(path, line = 0):
-    
+
+def readHurdat(path, line=0):
     with open(path) as f:
         hurdat_raw = f.readlines()
-    
+
     hurdat_final = {}
     while line < len(hurdat_raw):
         header = cleanInputLine(hurdat_raw[line])
-        hurdat_final[header[0]] = readStorm(header[0], header[1], hurdat_raw[line+1:line+1+int(header[2])])
+        hurdat_final[header[0]] = readStorm(header[0], header[1], hurdat_raw[line + 1:line + 1 + int(header[2])])
         line += 1 + int(header[2])
     return hurdat_final
 
-#All bounds are [min, max), and if the storm passes through the bounding box/timebox in at least 1 datapoint, it is included
-#Min and max bounds for latlon come from globe geometery, min time is 1700, since Hurdat like won't go back that far as it is before the United States, and max 
-#time is now since you can't see the future
-#Time bounds should be datetime objects, lat and lon should be floats in the range [-180, 180] for lon and [-90, 90] for lat
-#If makesLandfall is True, it also restricts it to only hurricanes that make landfall
-def trimHurdat(hurdat, 
-               lonMin = -180, lonMax = 180, 
-               latMin = -90, latMax = 91, 
-               timeMin = dt.datetime(1700, 1, 1, tzinfo = dt.timezone.utc), timeMax = dt.datetime.now(dt.timezone.utc)):
+
+# All bounds are [min, max), and if the storm passes through the bounding box/timebox in at least 1 datapoint,
+# it is included Min and max bounds for latlon come from globe geometery, min time is 1700, since Hurdat won't
+# go back that far as it is before the United States, and max time is now since you can't see the future. Time bounds
+# should be datetime objects, lat and lon should be floats in the range [-180, 180] for lon and [-90, 90] for lat
+def trimHurdat(hurdat,
+               lonMin=-180, lonMax=180,
+               latMin=-90, latMax=91,
+               timeMin=dt.datetime(1700, 1, 1, tzinfo=dt.timezone.utc), timeMax=dt.datetime.now(dt.timezone.utc)):
     trimmedHurdat = {}
     for key in hurdat:
-        for lon, lat, time in zip(hurdat[key]['data']['lon'], hurdat[key]['data']['lat'], hurdat[key]['data']['datetime']):
-            if lon <= lonMax and lon >= lonMin and lat >= latMin and lat <= latMax and time <= timeMax and time >= timeMin:
+        for lon, lat, time in zip(hurdat[key]['data']['lon'], hurdat[key]['data']['lat'],
+                                  hurdat[key]['data']['datetime']):
+            if lonMax >= lon >= lonMin and latMin <= lat <= latMax and timeMax >= time >= timeMin:
                 trimmedHurdat[key] = hurdat[key]
                 break
     return trimmedHurdat
--- PlotPaths.py
+++ PlotPaths.py
@@ -4,70 +4,77 @@ from matplotlib.patches import Ellipse
 import cartopy.crs as ccrs
 from geopy import distance
 import scipy.stats as st
-    
+
+
 def positionBeforeArrival(storm, tMinusTime, lon_min, lon_max, lat_min, lat_max):
     if type(tMinusTime) == int or type(tMinusTime) == float:
         tMinusTime = dt.timedelta(hours=-tMinusTime)
-    
+
     keyTime = None
     for i, (lon, lat) in enumerate(zip(storm['data']['lon'], storm['data']['lat'])):
-        if lon >= lon_min and lon <= lon_max and lat >= lat_min and lat <= lat_max:
+        if lon_min <= lon <= lon_max and lat_min <= lat <= lat_max:
             keyTime = storm['data']['datetime'][i] + tMinusTime
             break
-    
-    if keyTime is None: #this means it doesn't enter our bounding box and can't be plotted
+
+    if keyTime is None:  # this means it doesn't enter our bounding box and can't be plotted
         return
-    
+
     bounds = getSynopticBounds(storm, keyTime)
-    
-    if bounds is None: #this means we are too early/late in data and it can't be plotted
+
+    if bounds is None:  # this means we are too early/late in data and it can't be plotted
         return
-    
+
     if type(bounds) == int:
-        return ((storm['data']['lon'][bounds], storm['data']['lat'][bounds]), storm)
+        return (storm['data']['lon'][bounds], storm['data']['lat'][bounds]), storm
     else:
-        #god i hate spherical coordinates
-        velocity = (distance.distance((storm['data']['lat'][bounds[0]], storm['data']['lon'][bounds[0]]), 
-                                (storm['data']['lat'][bounds[1]], storm['data']['lon'][bounds[1]])).km / 
+        # god i hate spherical coordinates
+        velocity = (distance.distance((storm['data']['lat'][bounds[0]], storm['data']['lon'][bounds[0]]),
+                                      (storm['data']['lat'][bounds[1]], storm['data']['lon'][bounds[1]])).km /
                     (storm['data']['datetime'][bounds[1]] - storm['data']['datetime'][bounds[0]]).total_seconds())
         time = (keyTime - storm['data']['datetime'][bounds[0]]).total_seconds()
         stormDistance = velocity * time
         dest = distance.distance(kilometers=stormDistance).destination(
-            (storm['data']['lat'][bounds[0]], storm['data']['lon'][bounds[0]]), 
-            bearing = twoPointBearing(storm['data']['lon'][bounds[0]], storm['data']['lat'][bounds[0]],
-                                     storm['data']['lon'][bounds[1]], storm['data']['lat'][bounds[1]]))
-        return((dest.longitude, dest.latitude), storm)
+            (storm['data']['lat'][bounds[0]], storm['data']['lon'][bounds[0]]),
+            bearing=twoPointBearing(storm['data']['lon'][bounds[0]], storm['data']['lat'][bounds[0]],
+                                    storm['data']['lon'][bounds[1]], storm['data']['lat'][bounds[1]]))
+        return (dest.longitude, dest.latitude), storm
 
-#This gets the index or two indicies of the 2 hurricane observation steps bounding a given datetime
+
+# This gets the index or two indicies of the 2 hurricane observation steps bounding a given datetime
 def getSynopticBounds(storm, datetime):
-    for i in range(len(storm['data']['datetime']) -1):
+    for i in range(len(storm['data']['datetime']) - 1):
         if datetime == storm['data']['datetime'][i]:
             return i
-        if datetime == storm['data']['datetime'][i+1]:
-            return i+1
-        if datetime > storm['data']['datetime'][i] and datetime < storm['data']['datetime'][i + 1]:
-            return i, i+1
-        
-#This looks at the status of the storm at the first point it enters a lonlat bounding box, returns None if the path never crosses through that box
-def statusNearLocation(storm, lon_min=-180, lon_max=180, lat_min=-90, lat_max=90, returnTime = False):
+        if datetime == storm['data']['datetime'][i + 1]:
+            return i + 1  # here if the datapoint we want is the last one overall
+        if storm['data']['datetime'][i] < datetime < storm['data']['datetime'][i + 1]:
+            return i, i + 1
+
+
+# This looks at the status of the storm at the first point it enters a lonlat bounding box, returns None if the path
+# never crosses through that box
+def statusNearLocation(storm, lon_min=-180, lon_max=180, lat_min=-90, lat_max=90, returnTime=False):
     for i, (lon, lat) in enumerate(zip(storm['data']['lon'], storm['data']['lat'])):
-        if lon >= lon_min and lon <= lon_max and lat >= lat_min and lat <= lat_max:
+        if lon_min <= lon <= lon_max and lat_min <= lat <= lat_max:
             if not returnTime:
                 return storm['data']['system status'][i]
             else:
                 return storm['data']['system status'][i], storm['data']['datetime'][i]
 
+
 def lonLatToXYZ(lon, lat):
-    return (np.cos(np.pi / 180 * lon) * np.cos(np.pi / 180 * lat), 
+    return (np.cos(np.pi / 180 * lon) * np.cos(np.pi / 180 * lat),
             np.sin(np.pi / 180 * lon) * np.cos(np.pi / 180 * lat),
             np.sin(np.pi / 180 * lat))
 
+
 def XYZtoLonLat(x, y, z):
-    return (180 / np.pi * np.arctan2(y,x), 
+    return (180 / np.pi * np.arctan2(y, x),
             90 - 180 / np.pi * np.arctan2(np.sqrt(x ** 2 + y ** 2), z))
 
+
 def twoPointBearing(lon1, lat1, lon2, lat2):
-    x_bearing = (np.cos(np.pi / 180 * lat2) * 
+    x_bearing = (np.cos(np.pi / 180 * lat2) *
                  np.sin(np.pi / 180 * (lon2 - lon1)))
     y_bearing = (np.cos(np.pi / 180 * lat1) * np.sin(np.pi / 180 * lat2) -
                  (np.sin(np.pi / 180 * lat1) * np.cos(np.pi / 180 * lat2) *
@@ -75,8 +82,9 @@ def twoPointBearing(lon1, lat1, lon2, lat2):
     bearing = 180 / np.pi * np.arctan2(x_bearing, y_bearing)
     return bearing
 
-#from https://www.visiondummy.com/2014/04/draw-error-ellipse-representing-covariance-matrix/ 
-#and https://stackoverflow.com/questions/12301071/multidimensional-confidence-intervals
+
+# from https://www.visiondummy.com/2014/04/draw-error-ellipse-representing-covariance-matrix/
+# and https://stackoverflow.com/questions/12301071/multidimensional-confidence-intervals
 
 def cov_ellipse(cov, q=None, nsig=None, **kwargs):
     """
@@ -111,9 +119,11 @@ def cov_ellipse(cov, q=None, nsig=None, **kwargs):
 
     return width, height, rotation
 
-#potential todo: use a kent distribution rather than what I'm doing now
 
-def plotTMinusPosition(hurdat, tMinus, stormType, lon_min=-180, lon_max=180, lat_min=-90, lat_max=90, storm_alpha=0.5, bounds = None, saveFig = None):
+# potential todo: use a kent distribution rather than what I'm doing now
+
+def plotTMinusPosition(hurdat, tMinus, stormType, lon_min=-180, lon_max=180, lat_min=-90, lat_max=90, storm_alpha=0.5,
+                       bounds=None, saveFig=None):
     fig = plt.figure(figsize=(20, 10))
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
 
@@ -122,13 +132,13 @@ def plotTMinusPosition(hurdat, tMinus, stormType, lon_min=-180, lon_max=180, lat
         bounds = [-100, -30, 10, 90]
 
     ax.set_extent(bounds, crs=ccrs.Geodetic())
-    #ax.set_global()
+    # ax.set_global()
 
     lon_list = []
     lat_list = []
     storm_list = []
     status_list = []
-    
+
     for key in hurdat:
         position = positionBeforeArrival(hurdat[key], tMinus, lon_min, lon_max, lat_min, lat_max)
 
@@ -139,16 +149,15 @@ def plotTMinusPosition(hurdat, tMinus, stormType, lon_min=-180, lon_max=180, lat
 
     lon_plot = []
     lat_plot = []
-    
+
     for i, (sLon, sLat, storm) in enumerate(zip(lon_list, lat_list, storm_list)):
         if statusNearLocation(storm, lon_min, lon_max, lat_min, lat_max) in stormType:
-            ax.plot(sLon, sLat, 'o', transform=ccrs.Geodetic(), alpha = storm_alpha)
+            ax.plot(sLon, sLat, 'o', transform=ccrs.Geodetic(), alpha=storm_alpha)
             lon_plot.append(sLon)
             lat_plot.append(sLat)
-            
-            
-    lonLatList = np.stack([lon_plot, lat_plot], axis = 1)
-    
+
+    lonLatList = np.stack([lon_plot, lat_plot], axis=1)
+
     mean = np.mean(lonLatList, axis=0)
     cov = np.cov(lonLatList, rowvar=0)
     mvNorm = st.multivariate_normal(mean, cov=cov)
@@ -158,33 +167,33 @@ def plotTMinusPosition(hurdat, tMinus, stormType, lon_min=-180, lon_max=180, lat
     gridY = np.arange(bounds[2], bounds[3], STEP)
     meshX, meshY = np.meshgrid(gridX, gridY)
     pos = np.dstack([meshX, meshY])
-    
-    #ax.contourf(meshX, meshY, mvNorm.pdf(pos), transform=ccrs.PlateCarree(), levels = 10)
-    
+
+    # ax.contourf(meshX, meshY, mvNorm.pdf(pos), transform=ccrs.PlateCarree(), levels = 10)
+
     S3width, S3height, S3rotation = cov_ellipse(cov, nsig=3)
-    ax.add_patch(Ellipse(mean, S3width, S3height, transform=ccrs.Geodetic(), 
-                         angle=S3rotation, facecolor='White', edgecolor='red', label = '3 sigma interval'))
-    
+    ax.add_patch(Ellipse(mean, S3width, S3height, transform=ccrs.Geodetic(),
+                         angle=S3rotation, facecolor='White', edgecolor='red', label='3 sigma interval'))
+
     S2width, S2height, S2rotation = cov_ellipse(cov, nsig=2)
-    ax.add_patch(Ellipse(mean, S2width, S2height, transform=ccrs.Geodetic(), 
-                         angle=S2rotation, facecolor='White', edgecolor='blue', label = '2 sigma interval'))
-    
+    ax.add_patch(Ellipse(mean, S2width, S2height, transform=ccrs.Geodetic(),
+                         angle=S2rotation, facecolor='White', edgecolor='blue', label='2 sigma interval'))
+
     S1width, S1height, S1rotation = cov_ellipse(cov, nsig=1)
-    ax.add_patch(Ellipse(mean, S1width, S1height, transform=ccrs.Geodetic(), 
-                         angle=S1rotation, facecolor='White', edgecolor='green', label = '1 sigma interval'))
-    
-    ax.plot([lon_min, lon_max], [lat_min, lat_min], transform=ccrs.PlateCarree(), color = 'pink')
-    ax.plot([lon_min, lon_max], [lat_max, lat_max], transform=ccrs.PlateCarree(), color = 'pink')
-    ax.plot([lon_max, lon_max], [lat_min, lat_max], transform=ccrs.PlateCarree(), color = 'pink')
-    ax.plot([lon_min, lon_min], [lat_min, lat_max], transform=ccrs.PlateCarree(), color = 'pink', label = 'Bounding Box')
-        
+    ax.add_patch(Ellipse(mean, S1width, S1height, transform=ccrs.Geodetic(),
+                         angle=S1rotation, facecolor='White', edgecolor='green', label='1 sigma interval'))
+
+    ax.plot([lon_min, lon_max], [lat_min, lat_min], transform=ccrs.PlateCarree(), color='pink')
+    ax.plot([lon_min, lon_max], [lat_max, lat_max], transform=ccrs.PlateCarree(), color='pink')
+    ax.plot([lon_max, lon_max], [lat_min, lat_max], transform=ccrs.PlateCarree(), color='pink')
+    ax.plot([lon_min, lon_min], [lat_min, lat_max], transform=ccrs.PlateCarree(), color='pink', label='Bounding Box')
+
     ax.legend()
-        
-    ax.plot(*mean, '+', transform=ccrs.PlateCarree(), color = 'black')
+
+    ax.plot(*mean, '+', transform=ccrs.PlateCarree(), color='black')
 
     print(f'{len(lonLatList)} storms plotted')
-    
+
     if saveFig is not None:
-        plt.savefig(saveFig)    
-    
+        plt.savefig(saveFig)
+
     plt.show()
